91eb875b81b5b0c99abd77bd49a14d34ff4cf98f
William Jagels <will.jagels@gmail.com>
2014-11-26 01:34:28 -0500

    Clean up the method comments in Primes.cpp

--- CPrimes/Primes.cpp
+++ CPrimes/Primes.cpp
@@ -9,15 +9,14 @@ Primes::Primes() {
 Primes::~Primes() {
 }
 
-// Determines if parameter is a prime by the definition of a prime number.
+/*Returns true if parameter a is prime by the definition of a prime number.
+Sets failed to the first number that a was divisible by*/
 bool Primes::isPrime(unsigned long long int a, unsigned long long int *failed) {
 	if (a < 2)
 		return false;
 	if (a == 3 || a == 2)
 		return true;
 	for (size_t i = 2; i <= sqrt((double)a); i++) {
-		/*string tmp = "Checking " + std::to_string(a) + " mod " + std::to_string(i);
-		cout << tmp << endl; //for debugging */
 		if (a % i == 0) {
 			if (*failed != NULL)
 				*failed = i;
@@ -27,7 +26,7 @@ bool Primes::isPrime(unsigned long long int a, unsigned long long int *failed) {
 	return true;
 }
 
-//Simple, yet efficient.
+/*Returns true if the parameter a is divisible by any number at or under the square root of a in the vector parameter, primes.*/
 bool Primes::isPrime(unsigned long long int a, std::vector<unsigned long long int>* primes) {
 	if (a < 2)	//2 is the first prime
 		return false;
