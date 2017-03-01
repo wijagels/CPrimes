3c018ab53f8133dedc58bdd1438046ce821582a0
William Jagels <will.jagels@gmail.com>
2017-03-01 11:55:15 -0500

    Remove old naive c++ files

--- cprimes.h
+++
@@ -1,6 +0,0 @@
-#include <vector>
-#include <iostream>
-#include <limits.h>
-#include <string>
-void genPrimes(std::vector<unsigned long long int>* primes, unsigned long long int max);
-std::vector<unsigned long long int> primes;
